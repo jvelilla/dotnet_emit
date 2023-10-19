7aa69277c7ed85b31e1836539c3a19e9b349d29a
jvelilla <javier.hector@gmail.com>
2023-10-19 10:07:45 -0300

    Added C# basic generic example

---
+++ testing/cs/generics/basic_generics/GenericClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basic_generics;
+
+public class GenericClass<T>
+{
+    private T genericMember;
+
+    public GenericClass(T value)
+    {
+        genericMember = value;
+    }
+
+    public T GenericMethod(T genericParameter)
+    {
+        Console.WriteLine("Parameter type: {0}, value: {1}", typeof(T).ToString(), genericParameter);
+        Console.WriteLine("Return type: {0}, value: {1}", typeof(T).ToString(), genericMember);
+
+        return genericMember;
+    }
+
+    public T? GenericProperty { get; set; }
+}
+
---
+++ testing/cs/generics/basic_generics/Program.cs
@@ -0,0 +1,20 @@
+﻿using basic_generics;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+            // Instantiate the generic class with int as its type parameter
+        GenericClass<int> intGenericClass = new GenericClass<int>(10);
+
+        int val = intGenericClass.GenericMethod(200);
+        Console.WriteLine(val);
+
+        // Instantiate the generic class with string as its type parameter
+        GenericClass<string> stringGenericClass = new GenericClass<string>("Hello Eiffel");
+
+        string result = stringGenericClass.GenericMethod("Hello Eiffel.Net");
+
+        Console.WriteLine(result);
+    }
+}
